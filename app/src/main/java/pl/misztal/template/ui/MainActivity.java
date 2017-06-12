23c769d97d1535d3c6ce6d6b8b2ea47059c31121
Krzysztof Misztal <krmisztal@gmail.com>
2017-06-12 13:25:33 +0200

    Requesting permissions

--- app/src/main/java/pl/misztal/template/ui/MainActivity.java
+++ app/src/main/java/pl/misztal/template/ui/MainActivity.java
@@ -1,15 +1,13 @@
 package pl.misztal.template.ui;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.PermissionChecker;
-
-import java.security.Permissions;
 
 import pl.misztal.template.R;
 import pl.misztal.template.ui.base.BaseActivity;
@@ -25,33 +23,54 @@ import pl.misztal.template.ui.nearby.NearbyFragment;
 
 public class MainActivity extends BaseActivity<BaseView, DefaultPresenter> {
 
+    public static final int REQUEST_LOCATION_CODE = 1;
+
+    private boolean fragmentInited = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
-        if (fragment == null) {
-            fragment = NearbyFragment.newInstance();
-        }
-
-        if (!fragment.isAdded()) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, fragment)
-                    .commit();
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO: 12.06.2017 do it good
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-
+        if (permissionsGranted()) {
+            initFragment();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_CODE);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            finish();
+        }
+    }
+
+    private void initFragment() {
+        if (!fragmentInited) {
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+            if (fragment == null) {
+                fragment = NearbyFragment.newInstance();
+            }
+
+            if (!fragment.isAdded()) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.container, fragment)
+                        .commit();
+            }
+            fragmentInited = true;
+        }
+    }
+
+    private boolean permissionsGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
     }
 
     @NonNull
